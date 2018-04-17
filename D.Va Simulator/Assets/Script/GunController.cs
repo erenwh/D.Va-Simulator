bfa36f45f09a68d1d8054fe615b26f1d7a53197f
Han Wang <erenwh@gmail.com>
2018-04-17 01:05:50 -0400

    gun1 finished

---
+++ D.Va Simulator/Assets/Script/GunController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZEffects;
+
+public class GunController : MonoBehaviour {
+	public GameObject ControllerRight;
+
+	private SteamVR_TrackedObject trackObj;
+	private SteamVR_Controller.Device device;
+
+	private SteamVR_TrackedController controller;
+
+	public EffectTracer TracerEffect;
+	public Transform muzzleTransform;
+
+	// Use this for initialization
+	void Start () {
+		controller = ControllerRight.GetComponent<SteamVR_TrackedController>();
+		controller.TriggerClicked += TriggerPressed;
+		trackObj = ControllerRight.GetComponent<SteamVR_TrackedObject>();
+	}
+
+	private void TriggerPressed(object sender, ClickedEventArgs e) {
+		ShootWeapon();
+	}
+
+	public void ShootWeapon() {
+		RaycastHit hit = new RaycastHit();
+		Ray ray = new Ray(muzzleTransform.position, muzzleTransform.forward);
+
+		device = SteamVR_Controller.Input((int)trackObj.index);
+		device.TriggerHapticPulse(1500);
+		TracerEffect.ShowTracerEffect(muzzleTransform.position, muzzleTransform.forward, 250f);
+
+		if (Physics.Raycast(ray, out hit, 5000f)) {
+			if (hit.collider.attachedRigidbody) {
+				Debug.Log("hit" + hit.collider.gameObject.name);
+			}
+		}
+	}
+	
+}
---
+++ D.Va Simulator/Assets/SteamVR/Scripts/PlayerMechanics.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMechanics : MonoBehaviour {
+	
+	public GameObject MatrixCube;
+	/*private SteamVR_Controller.Device device;
+	private SteamVR_TrackedObject TrackedObject;
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+	private bool triggerButtonDown = false;
+	*/
+	public SteamVR_TrackedController device;
+	public SteamVR_TrackedController device2;
+
+	void Gripper(object sender, ClickedEventArgs e) {
+		Instantiate(MatrixCube, new Vector3(0,0,0), Quaternion.identity);
+		Debug.Log("something");
+	}
+
+	// Use this for initialization
+	void Start () {
+		//trackedObject = GetComponent<SteamVR_TrackedObject>();
+		device = GetComponent<SteamVR_TrackedController>();
+		device.Gripped += Gripper;
+		device2 = GetComponent<SteamVR_TrackedController>();
+		device2.Gripped += Gripper;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//device = SteamVR_Controller.Input((int)trackedObject.index);
+		/*if (device.GetAxis().x != 0) {
+
+		}*/
+		/*triggerButtonDown = controller.GetPressDown(triggerButton);
+		if (triggerButtonDown) {
+			// Fire
+		}
+		if (device.GetPressDown(SteamVR_Controller.ButtonMask.))
+		gripButtonDown = controller.GetPressDown()
+		if (Pre)*/
+	}
+}
