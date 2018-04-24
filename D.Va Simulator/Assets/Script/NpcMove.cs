8b23c10cd51e148404875490facab00c769478ab
rhbrian <brhee@purdue.edu>
2018-04-23 22:47:38 -0400

    Revert "Merge branch 'master' of https://github.com/erenwh/D.Va-Simulator"
    This reverts commit 3ab197637f7882fab2378b3f35beef97fce26686, reversing
    changes made to a5473c43987e5d2d890cc8967f43efa43c647828.

--- D.Va Simulator/Assets/Script/NpcMove.cs
+++ D.Va Simulator/Assets/Script/NpcMove.cs
@@ -5,15 +5,15 @@ using UnityEngine.AI;
 
 public class NpcMove : MonoBehaviour {
 
-
-    private Transform _destination;
+  
+    [SerializeField]
+    Transform _destination;
 
 	NavMeshAgent _navMeshAgent;
 
 	// Use this for initialization
 	void Start () {
 		_navMeshAgent = this.GetComponent<NavMeshAgent>();
-        
 		if (_navMeshAgent == null) {
 			Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
 		} else {
@@ -21,9 +21,7 @@ public class NpcMove : MonoBehaviour {
 		}
 	}
 	private void SetDestination() {
-        _destination = GameObject.FindGameObjectWithTag("Target1").transform;
         if (_destination != null) {
-           
             Vector3 targetVector = _destination.transform.position;
             _navMeshAgent.SetDestination(targetVector);
         }
